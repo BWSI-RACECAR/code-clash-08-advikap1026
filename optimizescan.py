db8f5c8f28d157bbcbb0052f94e9d54c58e7e91c
advikap1026 <127911640+advikap1026@users.noreply.github.com>
2023-07-21 09:25:10 -0400

    Add files via upload

--- optimizescan.py
+++ optimizescan.py
@@ -4,22 +4,26 @@ class Solution:
         # return type: float
 
         #TODO: Write code below to return a float with the solution to the prompt.
-        meters = float(battcap/250)
-        maxArea = 0
-        length = 0 
-        width = 0 
-        count = 1 
-        for i in range(int(meters)):
-            width = meters - count 
-            length = meters - 2*(width)
-            if width > 0 and length > 0: 
-                area = width *length 
-                if area > maxArea:
-                    maxArea = area
-            count = count + 1
-        return maxArea
-        pass 
-
+        # meters = float(battcap/250)
+        # maxArea = 0
+        # length = 0 
+        # width = 0 
+        # count = 1 
+        # for i in range(int(meters)):
+        #     width = meters - count 
+        #     length = meters - 2*(width)
+        #     if width > 0 and length > 0: 
+        #         area = width *length 
+        #         if area > maxArea:
+        #             maxArea = area
+        #     count = count + 1
+        # return maxArea
+        # pass 
+        return float((battcap/500)*(battcap/1000))
+    
+        #A = P^2/4 - 2(p/4)^2
+        #A = p - 4tdist 
+        #tdist = P/4
 def main():
     battcap = int(input())
 
